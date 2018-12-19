126a4af0a9d494dfbf6f55114a85594a5d259571
Morgan Segura <morgansegura@gmail.com>
2018-12-19 14:07:26 -0800

    Add favicons

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -18,22 +18,13 @@ class TemplateWrapper extends Component {
             <title>{config.siteTitleAlt}</title>
             <meta name="description" content={config.siteDescription} />
             <meta name="u2f-support" content="true" />
-            <link rel="apple-touch-icon" sizes="57x57" href="/img/apple-icon-57x57.png" />
-            <link rel="apple-touch-icon" sizes="60x60" href="/img/apple-icon-60x60.png" />
-            <link rel="apple-touch-icon" sizes="72x72" href="/img/apple-icon-72x72.png" />
-            <link rel="apple-touch-icon" sizes="76x76" href="/img/apple-icon-76x76.png" />
-            <link rel="apple-touch-icon" sizes="114x114" href="/img/apple-icon-114x114.png" />
-            <link rel="apple-touch-icon" sizes="120x120" href="/img/apple-icon-120x120.png" />
-            <link rel="apple-touch-icon" sizes="144x144" href="/img/apple-icon-144x144.png" />
-            <link rel="apple-touch-icon" sizes="152x152" href="/img/apple-icon-152x152.png" />
-            <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-icon-180x180.png" />
-            <link rel="icon" type="image/png" sizes="192x192"  href="/img/android-icon-192x192.png" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
             <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png" />
-            <link rel="icon" type="image/png" sizes="96x96" href="/img/favicon-96x96.png" />
             <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png" />
-            <link rel="manifest" href="/img/manifest.json" />
-            <meta name="msapplication-TileColor" content="#ffffff" />
-            <meta name="msapplication-TileImage" content="/img/ms-icon-144x144.png" />            
+            <link rel="manifest" href="/img/site.webmanifest" />
+            <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#5bbad5" />
+            <meta name="msapplication-TileColor" content="#da532c" />
+            <meta name="theme-color" content="#ffffff" />
           </Helmet>
           <Header config={config} />
           <main className="main">
